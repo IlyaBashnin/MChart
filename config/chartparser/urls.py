fcafcbec8bbf8296f427f5a6f6bc18337e39e104
IlyaBashnin <ilyabashnin@gmail.com>
2020-12-26 05:01:34 +0300

    Create some new urls (index, about, contacts, details)

--- config/chartparser/urls.py
+++ config/chartparser/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path, include
+from django.urls import path
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    re_path(r'^about', views.about),
+    re_path(r'^contact', views.contact),
+    re_path(r'^yamusic', views.yamusic_parsing),
+    re_path(r'^details', views.details),
 ]
