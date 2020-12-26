d6afd0659f0fc053856135b180c8affcb0d8160e
IlyaBashnin <ilyabashnin@gmail.com>
2020-12-26 07:29:56 +0300

    Change type of field. IntegerField -> PositiveSmallIntegerField

--- config/chartparser/models.py
+++ config/chartparser/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class YandexMusicChart(models.Model):
-    chart_position = models.IntegerField()
+    chart_position = models.PositiveSmallIntegerField()
     track_name = models.CharField(max_length=100)
     author_name = models.CharField(max_length=50)
