36998e214a67cd05bba73d59db1c3d609193b4b9
IlyaBashnin <ilyabashnin@gmail.com>
2020-12-26 05:04:12 +0300

    Create new function (index, about, contact, details)

--- config/chartparser/views.py
+++ config/chartparser/views.py
@@ -1,7 +1,42 @@
+from .models import YandexMusicChart
+from bs4 import BeautifulSoup
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
+from pprint import pprint
+import csv
+import requests
+
+
+# URL = 'https://music.yandex.by/chart/tracks'
 
 
 def index(request):
-    return HttpResponse("<h4>Hello</h4>")
+    data = {'header': 'Hello Django', 'message': 'Welcome to YMusiChart parser'}
+    return render(request, 'chartparser/index.html', context=data)
 
+
+def about(request):
+    return HttpResponse('<h2>About</h2>')
+
+
+def contact(request):
+    return HttpResponseRedirect('/about')
+
+
+def details(request):
+    return HttpResponsePermanentRedirect('/')
+
+
+def yamusic_parsing(request):
+    author_name = request.GET.get('author', '')
+    output = '<h2>Author:</h2><h3>{0}'.format(author_name)
+    return HttpResponse(output)
+
+
+# if __name__=="__main__":
+#     response = requests.get(URL)
+#     soup = BeautifulSoup(response.text, 'lxml')
+#     quotes = soup.find_all('div', class_='d-track__name')
+#     authors = soup.find_all('div', class_='d-track__meta')
+#     for i in range(0, len(quotes)):
+#         print(i+1, quotes[i].text, authors[i].text,  sep=' || ')
