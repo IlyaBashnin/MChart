b7228dd00b1346f9ff6ff15cd454b79f0a99c183
IlyaBashnin <ilyabashnin@gmail.com>
2020-12-26 05:55:06 +0300

    Use TemplateView

--- config/chartparser/urls.py
+++ config/chartparser/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.urls import re_path
+from django.views.generic import TemplateView
+
 from . import views
 
 urlpatterns = [
     path('', views.index),
-    re_path(r'^about', views.about),
-    re_path(r'^contact', views.contact),
+    re_path(r'^about', TemplateView.as_view(template_name="about.html")),
+    re_path(r'^contact', TemplateView.as_view(template_name="contact.html")),
     re_path(r'^yamusic', views.yamusic_parsing),
     re_path(r'^details', views.details),
 ]
