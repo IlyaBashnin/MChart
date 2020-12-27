b6d5c013240a09612d902db7ecac3cbfbd96d53d
IlyaBashnin <ilyabashnin@gmail.com>
2020-12-27 09:56:27 +0300

    ///

--- config/chartparser/views.py
+++ config/chartparser/views.py
@@ -1,19 +1,17 @@
 from .forms import YMusiChartForm
+from .models import YandexMusicChart
+from .services import yandex_music_chart_parsing
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
 
-from .models import YandexMusicChart
-from bs4 import BeautifulSoup
-import csv
-import requests
-
-
-# URL = 'https://music.yandex.by/chart/tracks'
-
 
 def index(request):
-    authors = ['ONE', 'TWO', 'THREE']
-    data = {'header': 'Hello', 'message': 'Welcome to YMusiChart parser', 'authors': authors}
+    authors = ['ONE', 'TWO', 'THREE', '66677', '77556']
+    yandex_music_chart_parsing()
+    # x = YandexMusicChart(chart_position='5', track_name='Track5', author_name='Author5')
+    # x.save()
+    y = YandexMusicChart.objects.order_by('chart_position')
+    data = {'header': 'Hello', 'message': 'Welcome to YMusiChart parser', 'authors': y}
     return render(request, 'index.html', context=data)
 
 
@@ -26,18 +24,11 @@ def yamusic_parsing(request):
         form = YMusiChartForm(request.POST)
         if form.is_valid():
             author_name = form.cleaned_data['author_name']
-            return HttpResponse('<h2>Looking {0}</h2>'.format(author_name))
+            y = YandexMusicChart.objects.filter(author_name = author_name)
+            data = {'header': 'Hello', 'message': 'Welcome to YMusiChart parser', 'authors': y}
+            return render(request, 'index.html', context=data)
         else:
             return HttpResponse('Invalid author name')
     else:
         form = YMusiChartForm()
         return render(request, 'chart.html', {'form': form})
-
-
-# if __name__=="__main__":
-#     response = requests.get(URL)
-#     soup = BeautifulSoup(response.text, 'lxml')
-#     quotes = soup.find_all('div', class_='d-track__name')
-#     authors = soup.find_all('div', class_='d-track__meta')
-#     for i in range(0, len(quotes)):
-#         print(i+1, quotes[i].text, authors[i].text,  sep=' || ')
