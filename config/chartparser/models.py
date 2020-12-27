801a42f307ea86254cac26c43fa013f951318e77
IlyaBashnin <ilyabashnin@gmail.com>
2020-12-27 09:54:18 +0300

    Add Meta class

--- config/chartparser/models.py
+++ config/chartparser/models.py
@@ -5,3 +5,10 @@ class YandexMusicChart(models.Model):
     chart_position = models.PositiveSmallIntegerField()
     track_name = models.CharField(max_length=100)
     author_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.author_name
+
+    class Meta:
+        verbose_name = 'Chart'
+        db_table = 'yandex_music_chart'
