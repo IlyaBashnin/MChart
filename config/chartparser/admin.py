0628818dc484b80b7f866afb8e7d42d10e51b424
IlyaBashnin <ilyabashnin@gmail.com>
2020-12-28 10:59:42 +0300

    Add Spotify and LastFM in DB

--- config/chartparser/admin.py
+++ config/chartparser/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import YandexMusicChart
+from .models import YandexMusicChart, SpotifyChart, LastFMChart
 
 admin.site.register(YandexMusicChart)
+admin.site.register(SpotifyChart)
+admin.site.register(LastFMChart)
