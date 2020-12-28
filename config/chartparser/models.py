bbaea76f39ff59cb2323bdb2dbed8c39387a4e05
IlyaBashnin <ilyabashnin@gmail.com>
2020-12-28 11:00:06 +0300

    Add Spotify and LastFM models in DB

--- config/chartparser/models.py
+++ config/chartparser/models.py
@@ -11,5 +11,33 @@ class YandexMusicChart(models.Model):
         return out
 
     class Meta:
-        verbose_name = 'Chart'
+        verbose_name = 'Yandex Music Chart'
         db_table = 'yandex_music_chart'
+
+
+class SpotifyChart(models.Model):
+    chart_position = models.PositiveSmallIntegerField()
+    track_name = models.CharField(max_length=100)
+    author_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        out = f'{self.chart_position}. | {self.author_name}  -  {self.track_name}'
+        return out
+
+    class Meta:
+        verbose_name = 'Spotyfy Chart'
+        db_table = 'spotify_chart'
+
+
+class LastFMChart(models.Model):
+    chart_position = models.PositiveSmallIntegerField()
+    track_name = models.CharField(max_length=100)
+    author_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        out = f'{self.chart_position}. | {self.author_name}  -  {self.track_name}'
+        return out
+
+    class Meta:
+        verbose_name = 'LastFM Chart'
+        db_table = 'lastfm_chart'
